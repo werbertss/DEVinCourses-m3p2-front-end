9ee3cd20bf4c71479c260060a24a8b0551247a02
get.Friday <gabriel.getdev@gmail.com>
2022-11-08 17:57:55 -0300

    Implementa componente ao módulo

---
+++ src/app/modules/user-auth/user-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pro-user-auth',
+  templateUrl: './user-auth.component.html',
+  styleUrls: ['./user-auth.component.scss']
+})
+export class UserAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
