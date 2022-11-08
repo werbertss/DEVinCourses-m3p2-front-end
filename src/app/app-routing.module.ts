33e78f6c9aefd502a4b8eb28844fcac603e83835
get.Friday <gabriel.getdev@gmail.com>
2022-11-08 18:39:41 -0300

    Feat: adiciona rota do UserAuthModule para App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // Rota provisória para visualização de componentes do módulo
+    path: '',
+    loadChildren: () =>
+      import('./modules/user-auth/user-auth.module').then((m) => m.UserAuthModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
