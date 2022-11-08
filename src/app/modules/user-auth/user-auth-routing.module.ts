ac62063eb22e67ef1bd8304ac6a9287991e431a2
get.Friday <gabriel.getdev@gmail.com>
2022-11-08 18:40:15 -0300

    Feat: Adiciona rota padrão do UserAuthModule

--- src/app/modules/user-auth/user-auth-routing.module.ts
+++ src/app/modules/user-auth/user-auth-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAuthComponent } from './user-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserAuthComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
