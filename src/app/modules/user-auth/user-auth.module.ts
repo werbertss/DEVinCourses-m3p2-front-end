c39c6e0c9d775c82a5a6ff0741a2851ed4635a10
get.Friday <gabriel.getdev@gmail.com>
2022-11-08 22:08:49 -0300

    Fix: Remove componente de input para forms desnecessário

--- src/app/modules/user-auth/user-auth.module.ts
+++ src/app/modules/user-auth/user-auth.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UserAuthRoutingModule } from './user-auth-routing.module';
 import { UserAuthComponent } from './user-auth.component';
-import { FormInputsComponent } from './form-inputs/form-inputs.component';
-
 
 @NgModule({
   declarations: [
     UserAuthComponent,
-    FormInputsComponent
   ],
   imports: [
     CommonModule,
