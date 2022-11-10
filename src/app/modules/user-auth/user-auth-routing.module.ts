f4cea7a2f718fcf7b0fa3450ba7bf382de40d791
get.Friday <gabriel.getdev@gmail.com>
2022-11-10 20:00:51 -0300

    Feat: Rota padrão para user-auth.module

--- src/app/modules/user-auth/user-auth-routing.module.ts
+++ src/app/modules/user-auth/user-auth-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAuthComponent } from './user-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserAuthComponent,
+    children: [
+      // Lugar para rotas internas do módulo
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
