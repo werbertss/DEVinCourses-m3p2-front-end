d93384977c491d99014c217c65cfc245c97dfb0a
get.Friday <gabriel.getdev@gmail.com>
2022-11-10 20:00:39 -0300

    Feat: Rota padrão para main.module

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      // Lugar para rotas internas do módulo
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
