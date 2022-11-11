105e5d695426701bfe2dde9a2aaf7343bcee6cc9
Edmilson Gomes <edmilsondmx@gmail.com>
2022-11-10 21:54:50 -0300

    fix: retira componentes do modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ItemTrainingComponent } from './components/item-training/item-training.component';
-import { HomeComponent } from './pages/home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TrainingComponent } from './pages/training/training.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
@@ -14,9 +11,6 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ItemTrainingComponent,
-    HomeComponent,
     TrainingComponent,
     PageNotFoundComponent,
   ],
