137cd97ab62f2017d364ae179f33fb26997d24c4
get.Friday <gabriel.getdev@gmail.com>
2022-11-12 13:34:13 -0300

    Feat: Implementa métodos em user.service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import SERVER_ROUTE from '../../constants/server';
+import { Observable } from 'rxjs';
+import { IUser } from 'src/app/models/IUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getUserById(id: string): Observable<IUser> {
+    return this.http.get<IUser>(SERVER_ROUTE + id);
+  }
+
+  addUser(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>(SERVER_ROUTE, user);
+  }
+
+  editUser(id: string, user: IUser): Observable<IUser> {
+    return this.http.put<IUser>(SERVER_ROUTE + id, user)
+  }
 }
