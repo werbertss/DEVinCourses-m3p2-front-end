c04aa06c4c07f14e784f5547fe687aad105a60a7
get.Friday <gabriel.getdev@gmail.com>
2022-11-12 13:34:38 -0300

    Feat: Verificação login em authentication.service

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { iif, Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import SERVER_ROUTE from 'src/app/constants/server';
+import { ILogin } from '../../models/ILogin';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +13,27 @@ export class AuthenticationService {
   isLoggedIn: boolean = false;
   userAuthorized: boolean = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  verifyLogin(email: string, password: string): Observable<boolean> {
+    this.getUserLogin(email)
+      .subscribe((credentials) => {
+        if (
+          email == credentials.email &&
+          password == credentials.password
+        ){
+          this.userAuthorized = true;
+        }
+      });
+
+    return iif(
+      () => this.userAuthorized,
+      of(true).pipe(tap(() => this.isLoggedIn = true)),
+      of(false).pipe(tap(() => this.isLoggedIn = false))
+    )
+  }
+
+  private getUserLogin(email: string): Observable<ILogin> {
+   return this.http.get<ILogin>(SERVER_ROUTE + email);
+  }
 }
