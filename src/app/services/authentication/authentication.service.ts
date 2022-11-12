40222b17b9e0342d99187f3cddfc2e62df4f0dda
get.Friday <gabriel.getdev@gmail.com>
2022-11-12 12:18:34 -0300

    Feat: Adiciona atributos em authentication service

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import SERVER_ROUTE from '../../constants/server';
+import { Observable } from 'rxjs';
+import { IUser } from 'src/app/models/IUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  redirectUrl: string = '';
+  isLoggedIn: boolean = false;
+  userAuthorized: boolean = false;
 
   constructor(private http: HttpClient) { }
 }
