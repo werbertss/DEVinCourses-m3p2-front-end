2f52e9f35dd7b0b51d0f667691661c8850c6fe3b
get.Friday <gabriel.getdev@gmail.com>
2022-11-12 11:22:07 -0300

    Feat: Importa HttpClient e SERVER_ROUTE

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import SERVER_ROUTE from '../../constants/server';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
