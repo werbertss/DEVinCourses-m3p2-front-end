c382da8fe2f5428f9c9095b3781597d372e03f86
get.Friday <gabriel.getdev@gmail.com>
2022-11-12 12:37:14 -0300

    Fix: remove funções para user service

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import SERVER_ROUTE from '../../constants/server';
-import { Observable } from 'rxjs';
-import { IUser } from 'src/app/models/IUser';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +8,5 @@ export class AuthenticationService {
   isLoggedIn: boolean = false;
   userAuthorized: boolean = false;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 }
