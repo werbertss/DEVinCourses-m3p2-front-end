b41e3f781ba39bdd9a5a84d8012e2c6128a200d1
get.Friday <gabriel.getdev@gmail.com>
2022-11-14 16:30:58 -0300

    Fix: Formatação arquivo

--- src/app/modules/user-auth/registration/registration.component.ts
+++ src/app/modules/user-auth/registration/registration.component.ts
@@ -7,7 +7,7 @@ import {
   FormControl,
   FormGroup,
   ValidatorFn,
-  Validators
+  Validators,
 } from '@angular/forms';
 
 @Component({
@@ -15,40 +15,43 @@ import {
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss'],
 })
-
 export class RegistrationComponent implements OnInit {
   User!: IUser;
   selectedFile: any;
   myimage: any;
-  title='project'
+  title = 'project';
   confirmPassword: any = true;
-  formRegistration = new FormGroup(
-    {
-      nameInput:new FormControl(""),
-      emailInput:new FormControl(""),
-      ageInput:new FormControl(""),
-      cpfInput:new FormControl(""),
-      passowerInput: new FormControl("")
-    }
-  );
+  formRegistration = new FormGroup({
+    nameInput: new FormControl(''),
+    emailInput: new FormControl(''),
+    ageInput: new FormControl(''),
+    cpfInput: new FormControl(''),
+    passowerInput: new FormControl(''),
+  });
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.formRegistration = this.formBuilder.group(
       {
-        nameInput: ['', [Validators.required, Validators.minLength(3),this.checkNumbers]],
-        emailInput: ['',[Validators.required, Validators.email]],
-        ageInput: ['',[Validators.required, Validators.min(18), Validators.max(110)]],
-        cpfInput: ['',[Validators.required, this.checkCPF]],
-        passwordInput: ['',[Validators.required, Validators.minLength(8)]],
-        confirmInput: ['',[Validators.required]]
+        nameInput: [
+          '',
+          [Validators.required, Validators.minLength(3), this.checkNumbers],
+        ],
+        emailInput: ['', [Validators.required, Validators.email]],
+        ageInput: [
+          '',
+          [Validators.required, Validators.min(18), Validators.max(110)],
+        ],
+        cpfInput: ['', [Validators.required, this.checkCPF]],
+        passwordInput: ['', [Validators.required, Validators.minLength(8)]],
+        confirmInput: ['', [Validators.required]],
       },
       {
         validators: [this.checkMatch('passwordInput', 'confirmInput')],
       }
-    )
+    );
   }
 
   checkMatch(controlName: string, checkControlName: string): ValidatorFn {
@@ -90,8 +93,10 @@ export class RegistrationComponent implements OnInit {
   checkNumbers(dataForm: FormControl) {
     const inputValue = dataForm.value;
     const regex = /[0-9]/;
-    if (inputValue && inputValue!='') {
-      return regex.test(inputValue) ? {checkInvalid: true, actual: inputValue} : null;
+    if (inputValue && inputValue != '') {
+      return regex.test(inputValue)
+        ? { checkInvalid: true, actual: inputValue }
+        : null;
     }
     return null;
   }
@@ -100,7 +105,7 @@ export class RegistrationComponent implements OnInit {
     const inputPassword = dataForm.get('passwordInput');
     const inputConfirm = dataForm.get('confirmInput');
     if (inputPassword !== inputConfirm) {
-      return {checkInvalid: true, actual: inputConfirm};
+      return { checkInvalid: true, actual: inputConfirm };
     }
     return null;
   }
@@ -111,34 +116,37 @@ export class RegistrationComponent implements OnInit {
     Sum = 0;
     let stringCPF = dataForm.value;
 
-    if (stringCPF == "00000000000") return { cpfInvalido: true, atual: stringCPF };
+    if (stringCPF == '00000000000')
+      return { cpfInvalido: true, atual: stringCPF };
 
-    for (let i=1; i<=9; i++) Sum = Sum + parseInt(stringCPF.substring(i-1, i)) * (11 - i);
+    for (let i = 1; i <= 9; i++)
+      Sum = Sum + parseInt(stringCPF.substring(i - 1, i)) * (11 - i);
     Remainder = (Sum * 10) % 11;
 
-      if ((Remainder == 10) || (Remainder == 11))  Remainder = 0;
-      if (Remainder != parseInt(stringCPF.substring(9, 10)) ) return { cpfInvalid: true, actual: stringCPF };
+    if (Remainder == 10 || Remainder == 11) Remainder = 0;
+    if (Remainder != parseInt(stringCPF.substring(9, 10)))
+      return { cpfInvalid: true, actual: stringCPF };
 
     Sum = 0;
-      for (let i = 1; i <= 10; i++) Sum = Sum + parseInt(stringCPF.substring(i-1, i)) * (12 - i);
-      Remainder = (Sum * 10) % 11;
+    for (let i = 1; i <= 10; i++)
+      Sum = Sum + parseInt(stringCPF.substring(i - 1, i)) * (12 - i);
+    Remainder = (Sum * 10) % 11;
 
-      if ((Remainder == 10) || (Remainder == 11))  Remainder = 0;
-      if (Remainder != parseInt(stringCPF.substring(10, 11) ) ) return { cpfInvald: true, actual: stringCPF };
-      return null;
+    if (Remainder == 10 || Remainder == 11) Remainder = 0;
+    if (Remainder != parseInt(stringCPF.substring(10, 11)))
+      return { cpfInvald: true, actual: stringCPF };
+    return null;
   }
 
   onChange($event: any) {
     if ($event.target.files[0].size >= 10240000) {
-      window.alert("tamanho não é permitido");
+      window.alert('tamanho não é permitido');
       $event.target.value = null;
-
     } else {
       const file = $event.target.files[0];
       this.convertToBase64(file);
       //console.log(this.myimage);
     }
-
   }
 
   convertToBase64(file: File) {
