72f1d3931c7d474619a88ba6ac50595b8ff4b05c
get.Friday <gabriel.getdev@gmail.com>
2022-11-14 10:11:08 -0300

    Fix: chama verifyLogin com subscribe

--- src/app/modules/user-auth/login-form/login-form.component.ts
+++ src/app/modules/user-auth/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
@@ -6,37 +6,35 @@ import { AuthenticationService } from 'src/app/services/authentication/authentic
 @Component({
   selector: 'pro-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent implements OnInit {
-
+export class LoginFormComponent {
   regexPassword: RegExp = /[^!@#$ %^&*()_+\-=\[\]{};':"\\|,.<>\/?]$/;
 
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.pattern(this.regexPassword)]),
+  });
 
-  })
-
-  constructor(private authService: AuthenticationService, private route: Router) {}
-
-  ngOnInit(): void {}
+  constructor(private authService: AuthenticationService,private route: Router) {}
 
   submitLogin() {
     if (this.form.invalid) {
       return;
-
     }
 
     let email = this.form.controls['email'].value;
     let password = this.form.controls['password'].value;
-    /*
-    TODO: FIX AUTHENTICATION SERVICE
-    if (this.authService.verifyLogin(email, password)){
-      this.route.navigateByUrl('home');
-    } else {
-      this.authService.isLoggedIn = false;
-    }
-    */
+
+    this.authService.verifyLogin(email, password).subscribe({
+      next: (token) => {
+        localStorage.setItem('token', token);
+        this.authService.userAuthorized = true;
+        this.route.navigateByUrl('home');
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
