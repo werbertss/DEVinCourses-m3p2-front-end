3dc1431d49066aa73d0490d5d6709d9aed4c9058
get.Friday <gabriel.getdev@gmail.com>
2022-11-20 10:22:38 -0300

    Fix: Altera conexão para template string

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -11,18 +11,18 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUser(token: string): Observable<IUser> {
-    return this.http.get<IUser>(SERVER_ROUTE + token);
+    return this.http.get<IUser>(`${SERVER_ROUTE}/${token}`);
   }
 
   addUser(user: IUser): void {
-    this.http.post(SERVER_ROUTE + 'api/Users', user);
+    this.http.post(`${SERVER_ROUTE}/api/Users`, user);
   }
 
   editUser(id: string, user: IUser): void {
-    this.http.put<IUser>(SERVER_ROUTE + 'api/Users/' + id, user);
+    this.http.put<IUser>(`${SERVER_ROUTE}/api/Users/${id}`, user);
   }
 
   sendEmail(email: string): void {
-    this.http.post<string>(SERVER_ROUTE + 'api/Users/reset', email);
+    this.http.post<string>(`${SERVER_ROUTE}/api/Users/reset`, email);
   }
 }
