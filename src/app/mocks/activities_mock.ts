fc5b893f8506abcd347761c24d4bdacdbeea7569
Conrado Fortunato <conradofortunato246@gmail.com>
2022-11-20 14:34:58 -0300

    feat: cria mock para teste das atividades

---
+++ src/app/mocks/activities_mock.ts
@@ -0,0 +1,19 @@
+import { IActivitie } from "../models/activitie"
+
+export const ACTIVITIES_MOCK: IActivitie[] = [
+  {
+    id: 1,
+    title: "Primeira Atividade",
+    description: "Está é a primeira atividade do curso \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: 2,
+    title: "Segunda Atividade",
+    description: "Está é a segunda atividade do curso \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: 3,
+    title: "Terceira Atividade",
+    description: "Está é a terceira atividade do curso \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+]
