40c20bb9751a148ed6acb9ba9ad7f890a6246203
Conrado Fortunato <conradofortunato246@gmail.com>
2022-11-20 20:35:05 -0300

    feat: implementa modal de criação de atividade

--- src/app/modules/main/pages/activities/activities.component.ts
+++ src/app/modules/main/pages/activities/activities.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IActivitie } from 'src/app/models/activitie';
 import { ACTIVITIES_MOCK } from 'src/app/mocks/activities_mock';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TrainingService } from 'src/app/services/training/training.service';
 
 @Component({
   selector: 'pro-activities',
@@ -10,10 +13,38 @@ import { ACTIVITIES_MOCK } from 'src/app/mocks/activities_mock';
 export class ActivitiesComponent implements OnInit {
 
   activities: IActivitie[] = ACTIVITIES_MOCK;
+  formActivitie: FormGroup = new FormGroup({});
 
-  constructor() { }
+  page = 1;
+  pageSize = 20;
+  newActivitie: IActivitie = {
+      title: "",
+      description: ""
+  }
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal, private formBuilder: FormBuilder, private trainingService:TrainingService) {
+		config.backdrop = 'static';
+		config.keyboard = false;
+
+    this.formActivitie = formBuilder.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+  });
+	}
 
   ngOnInit(): void {
   }
 
+  open(content: any) {
+		this.modalService.open(content);
+	}
+
+  CreateActivitie(){
+    this.newActivitie.title = this.formActivitie.value.title
+    this.newActivitie.description = this.formActivitie.value.description
+    // Chama função na service para executar o post
+    this.trainingService.postActivitie(this.newActivitie);
+    console.log(this.formActivitie);
+    this.modalService.dismissAll();
+  }
 }
