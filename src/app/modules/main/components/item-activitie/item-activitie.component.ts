fd1d2266714edc55912cb138c5e63709e646c859
Conrado Fortunato <conradofortunato246@gmail.com>
2022-11-20 22:15:48 -0300

    feat: implementa função de edição e deleção de atividade

--- src/app/modules/main/components/item-activitie/item-activitie.component.ts
+++ src/app/modules/main/components/item-activitie/item-activitie.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { IActivitie } from 'src/app/models/activitie';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivitiesService } from 'src/app/services/activities/activities.service';
 
 
 @Component({
@@ -17,15 +19,31 @@ export class ItemActivitieComponent implements OnInit {
     description: ''
   }
 
+  formEdit: FormGroup = new FormGroup({});
+
   open(content: any) {
 		this.modalService.open(content);
 	}
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+  constructor(config: NgbModalConfig, private modalService: NgbModal, private formBuilder: FormBuilder, private activitiesService: ActivitiesService) {
 		config.backdrop = 'static';
 		config.keyboard = false;
+
+    this.formEdit = formBuilder.group({
+      description: ['', [Validators.required]],
+  });
   	}
 
   ngOnInit(): void {
+  }
+
+  editActivitie(){
+    this.item.description = this.formEdit.value.description;
+    this.activitiesService.putActivities(this.item.title, this.item);
+    this.modalService.dismissAll();
+  }
+
+  deleteAtivitie(title:string){
+    this.activitiesService.deleteActivitie(title);
   }
 }
